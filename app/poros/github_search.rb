81fd60626cdd8cb5948576f5eb6bdce77e947eb9
Ryan Allen <rallenwasd@gmail.com>
2020-02-06 14:45:24 -0700

    Add followers to github search
    Co-Authored-By: Melissa Robbins <mel-rob@users.noreply.github.com>

--- app/poros/github_search.rb
+++ app/poros/github_search.rb
@@ -14,4 +14,15 @@ class GithubSearch
       Repo.new(repo_data)
     end
   end
+
+  def followers
+    @followers ||= get_followers
+  end
+
+  def get_followers
+    service = GithubService.new
+    service.followers_by_token(@github_token).map do |follower_data|
+      Follower.new(follower_data)
+    end
+  end
 end

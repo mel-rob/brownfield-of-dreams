15da7f8e40520f4a4e8b92553125958e7d98ffd1
Ryan Allen <rallenwasd@gmail.com>
2020-02-06 13:36:06 -0700

    Refactor abstraction of github_service methods
    Co-Authored-By: Melissa Robbins <mel-rob@users.noreply.github.com>

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -1,12 +1,22 @@
 class GithubService
-
+  # OPTIMIZE: should github_token be an attribute of GitHubService?
+  # OPTIMIZE: why is repos_by_token an instance method and not class method?
+  
   def repos_by_token(github_token)
-    conn = Faraday.new(url: 'https://api.github.com') do |faraday|
-      faraday.headers["Authorization"] = "token #{github_token}"
+    @github_token = github_token
+    get_json("/user/repos?page=1&per_page=5")
+  end
+
+  private
+  def get_json(url)
+    response = conn.get(url)
+    json_response = JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def conn
+    Faraday.new(url: 'https://api.github.com') do |faraday|
+      faraday.headers["Authorization"] = "token #{@github_token}"
       faraday.adapter Faraday.default_adapter
     end
-
-    response = conn.get("/user/repos?page=1&per_page=5")
-    json_response = JSON.parse(response.body, symbolize_names: true)
   end
 end

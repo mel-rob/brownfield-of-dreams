b489b829ab6e7f591d9e8e84c776d67363fe258c
Ryan Allen <rallenwasd@gmail.com>
2020-02-08 11:52:55 -0700

    Add before action to produce flash message when applicable

--- app/controllers/tutorials_controller.rb
+++ app/controllers/tutorials_controller.rb
@@ -1,8 +1,18 @@
 # frozen_string_literal: true
 
 class TutorialsController < ApplicationController
+  before_action :user_authentication, only: :show
+
   def show
     tutorial = Tutorial.find(params[:id])
     @facade = TutorialFacade.new(tutorial, params[:video_id])
+  end
+
+  private
+
+  def user_authentication
+    unless current_user
+      flash.now[:notice] = 'User must login to bookmark videos'
+    end
   end
 end

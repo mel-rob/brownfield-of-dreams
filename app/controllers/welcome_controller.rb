39d0201b660244842d5e2cee542fcfa3af09eb1b
Ryan Allen <rallenwasd@gmail.com>
2020-02-09 15:08:44 -0700

    Add conditional for if current_user to welcome view index

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,10 +2,28 @@
 
 class WelcomeController < ApplicationController
   def index
+    if current_user
+      user_tutorials
+    else
+      visitor_tutorials
+    end
+  end
+
+  private
+
+  def user_tutorials
     if params[:tag]
       @tutorials = Tutorial.tagged_with(params[:tag]).paginate(page: params[:page], per_page: 5)
     else
       @tutorials = Tutorial.all.paginate(page: params[:page], per_page: 5)
+    end
+  end
+
+  def visitor_tutorials
+    if params[:tag]
+      @tutorials = Tutorial.where(classroom: false).tagged_with(params[:tag]).paginate(page: params[:page], per_page: 5)
+    else
+      @tutorials = Tutorial.where(classroom: false).paginate(page: params[:page], per_page: 5)
     end
   end
 end

16c0da8d2a58b949de0fb01404c122eea12d70f9
Melissa Robbins <melissamrobbins@gmail.com>
2020-02-09 21:53:27 -0700

    Add test for email uniqueness on visitor registration

--- spec/features/vistors/visitor_can_register_spec.rb
+++ spec/features/vistors/visitor_can_register_spec.rb
@@ -24,7 +24,7 @@ describe 'vister can create an account', :js do
     fill_in 'user[password]', with: password
     fill_in 'user[password_confirmation]', with: password
 
-    click_on'Create Account'
+    click_on 'Create Account'
 
     expect(current_path).to eq(dashboard_path)
 
@@ -32,5 +32,28 @@ describe 'vister can create an account', :js do
     expect(page).to have_content(first_name)
     expect(page).to have_content(last_name)
     expect(page).to_not have_content('Sign In')
+  end
+
+  it 'is not able to create an account if the username already exists' do
+    create(:user, email: 'user@email.com')
+
+    email = 'user@email.com'
+    first_name = 'Jim'
+    last_name = 'Bob'
+    password = 'password'
+    password_confirmation = 'password'
+
+    visit new_user_path
+
+    fill_in 'user[email]', with: email
+    fill_in 'user[first_name]', with: first_name
+    fill_in 'user[last_name]', with: last_name
+    fill_in 'user[password]', with: password
+    fill_in 'user[password_confirmation]', with: password
+
+    click_on 'Create Account'
+
+    expect(current_path).to eq(register_path)
+    expect(page).to have_content('Username already exists')
   end
 end

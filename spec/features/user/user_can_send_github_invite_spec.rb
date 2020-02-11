5031b8d747ce5955976cb4ea8df316e3dcf6bf3e
Ryan Allen <rallenwasd@gmail.com>
2020-02-11 11:48:51 -0700

    Add feature tests for user sending github email invite

--- spec/features/user/user_can_send_github_invite_spec.rb
+++ spec/features/user/user_can_send_github_invite_spec.rb
@@ -11,4 +11,35 @@ RSpec.describe 'user invite page' do
 
     expect(current_path).to eq('/invite')
   end
+
+  it 'user can send an email to a valid github user' do
+    user = create(:user)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit(github_invite_path)
+
+    fill_in('Github Handle').with ('rallen20')
+
+    click_on('Send Invitiation')
+
+    expect(current_path).to eq(dashboard_path)
+    within('.flash-message') do
+      expect(page).to have_content('Successfully sent invite!')
+    end
+
+  it 'user can not send an email to an invalid github user' do
+    user = create(:user)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit(github_invite_path)
+
+    fill_in('Github Handle').with ('XXrallen20XX')
+
+    click_on('Send Invitiation')
+
+    expect(current_path).to eq(dashboard_path)
+    within('.flash-message') do
+      expect(page).to have_content("The Github user you selected doesn't have an email address associated with their account.")
+    end
+  end
 end

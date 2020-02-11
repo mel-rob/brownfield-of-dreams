a0df7144c9953f969081792fd0e2afe1f0f1ea66
Ryan Allen <rallenwasd@gmail.com>
2020-02-11 15:39:24 -0700

    Add invite github user method to activation notifier mailer

--- app/mailers/activation_notifier_mailer.rb
+++ app/mailers/activation_notifier_mailer.rb
@@ -3,4 +3,9 @@ class ActivationNotifierMailer < ApplicationMailer
     @user = user
     mail(to: user.email, subject: 'Please activate your account.')
   end
+
+  def invite_github_user(to_email_address, github_username)
+    @github_username = github_username
+    mail(to: to_email_address, subject: 'Brownfield-of-Dreams Invitation')
+  end
 end

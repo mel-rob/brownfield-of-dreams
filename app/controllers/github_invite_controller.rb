34a1ebc36c64b42bfae3b75feb8184cd21e55f93
Ryan Allen <rallenwasd@gmail.com>
2020-02-11 14:23:40 -0700

    Add github invite create action to controller

--- app/controllers/github_invite_controller.rb
+++ app/controllers/github_invite_controller.rb
@@ -1,5 +1,16 @@
 class GithubInviteController < ApplicationController
   def new
+  end
 
+  def create
+    service = GithubService.new(current_user.github_token)
+    to_email_address = service.email_by_username(params['github_username'])
+    if to_email_address 
+      # ActivationNotifierMailer.invite_github_user(@user).deliver
+      flash[:notice] = 'Successfully sent invite!'
+    else
+      flash[:error] = "The Github user you selected doesn't have an email address associated with their account."
+    end
+    redirect_to(dashboard_path)
   end
 end

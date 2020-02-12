22bd13bb6c6cc6e3076726cb32b205201cc3a8e7
Ryan Allen <rallenwasd@gmail.com>
2020-02-11 17:00:52 -0700

    Refactor github invite controller to send three arguments

--- app/controllers/github_invite_controller.rb
+++ app/controllers/github_invite_controller.rb
@@ -6,7 +6,7 @@ class GithubInviteController < ApplicationController
     service = GithubService.new(current_user.github_token)
     to_email_address = service.email_by_username(params['github_username'])
     if to_email_address
-      ActivationNotifierMailer.invite_github_user(to_email_address, params['github_username']).deliver
+      ActivationNotifierMailer.invite_github_user(current_user.github_username, to_email_address, params['github_username']).deliver
       flash[:notice] = 'Successfully sent invite!'
     else
       flash[:error] = "The Github user you selected doesn't have an email address associated with their account."

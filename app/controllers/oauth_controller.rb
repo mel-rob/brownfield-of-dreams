d65207c42527646a400ac9387580e8e665a96fd6
Ryan Allen <rallenwasd@gmail.com>
2020-02-11 17:00:38 -0700

    Refactor oauth to also save github username

--- app/controllers/oauth_controller.rb
+++ app/controllers/oauth_controller.rb
@@ -1,12 +1,16 @@
 class OauthController < ApplicationController
   def create
-    current_user.update(github_token: token)
+    current_user.update(github_token: token_retrieval, github_username: username_retrieval)
     redirect_to dashboard_path
   end
 
   private
 
-  def token
+  def token_retrieval
     request.env['omniauth.auth']['credentials']['token']
+  end
+
+  def username_retrieval
+    request.env['omniauth.auth']['info']['nickname']
   end
 end

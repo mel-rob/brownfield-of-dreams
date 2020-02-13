4891aba91987890048fe389bbe88dd41805484b5
Ryan Allen <rallenwasd@gmail.com>
2020-02-13 11:27:40 -0700

    Comment out unused get started controller

--- app/controllers/get_started_controller.rb
+++ app/controllers/get_started_controller.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
-class GetStartedController < ApplicationController
-  def show; end
-end
+# class GetStartedController < ApplicationController
+#   def show; end
+# end
